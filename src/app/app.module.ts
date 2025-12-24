d16dea8ed10366a0935ef61f7b7e042aa6403b56
hajifullstack@02 <haji.fullstack@gmail.com>
2025-12-24 16:06:40 +0530

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TemplateApprochComponent } from './template-approch/template-approch.component';
 import { ReactiveApprochComponent } from './reactive-approch/reactive-approch.component';
+import { PipeDemoComponent } from './pipe-demo/pipe-demo.component';
+import { LongTextPipe } from './long-text.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateApprochComponent,
     ReactiveApprochComponent,
+    PipeDemoComponent,
+    LongTextPipe,
    
   ],
   imports: [
